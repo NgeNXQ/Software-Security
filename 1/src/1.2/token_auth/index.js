1ef8b6a175438cb643b5dfae76d50d04af3286f1
Babich Denys <denis.babich.acc@gmail.com>
2024-09-20 01:40:42 +0300

    Labwork1 final commit

--- 1/src/1.2/token_auth/index.js
+++ 1/src/1.2/token_auth/index.js
@@ -1,103 +1,16 @@
-const uuid = require('uuid');
-const express = require('express');
-const onFinished = require('on-finished');
-const bodyParser = require('body-parser');
 const path = require('path');
-const port = 3000;
-const fs = require('fs');
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+
+const JWT_SECRET_KEY = 'NUKE';
+const JWT_EXPIRATION = '30s';
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const SESSION_KEY = 'Authorization';
-
-class Session {
-    #sessions = {}
-
-    constructor() {
-        try {
-            this.#sessions = fs.readFileSync('./sessions.json', 'utf8');
-            this.#sessions = JSON.parse(this.#sessions.trim());
-
-            console.log(this.#sessions);
-        } catch(e) {
-            this.#sessions = {};
-        }
-    }
-
-    #storeSessions() {
-        fs.writeFileSync('./sessions.json', JSON.stringify(this.#sessions), 'utf-8');
-    }
-
-    set(key, value) {
-        if (!value) {
-            value = {};
-        }
-        this.#sessions[key] = value;
-        this.#storeSessions();
-    }
-
-    get(key) {
-        return this.#sessions[key];
-    }
-
-    init(res) {
-        const sessionId = uuid.v4();
-        this.set(sessionId);
-
-        return sessionId;
-    }
-
-    destroy(req, res) {
-        const sessionId = req.sessionId;
-        delete this.#sessions[sessionId];
-        this.#storeSessions();
-    }
-}
-
-const sessions = new Session();
-
-app.use((req, res, next) => {
-    let currentSession = {};
-    let sessionId = req.get(SESSION_KEY);
-
-    if (sessionId) {
-        currentSession = sessions.get(sessionId);
-        if (!currentSession) {
-            currentSession = {};
-            sessionId = sessions.init(res);
-        }
-    } else {
-        sessionId = sessions.init(res);
-    }
-
-    req.session = currentSession;
-    req.sessionId = sessionId;
-
-    onFinished(req, () => {
-        const currentSession = req.session;
-        const sessionId = req.sessionId;
-        sessions.set(sessionId, currentSession);
-    });
-
-    next();
-});
-
-app.get('/', (req, res) => {
-    if (req.session.username) {
-        return res.json({
-            username: req.session.username,
-            logout: 'http://localhost:3000/logout'
-        })
-    }
-    res.sendFile(path.join(__dirname+'/index.html'));
-})
-
-app.get('/logout', (req, res) => {
-    sessions.destroy(req, res);
-    res.redirect('/');
-});
+const port = 3000;
 
 const users = [
     {
@@ -112,26 +25,51 @@ const users = [
     }
 ]
 
+app.use((req, res, next) => {
+    const token = req.headers['authorization'];
+
+    if (token) {
+        jwt.verify(token, JWT_SECRET_KEY, (err, decoded) => {
+            if (err) {
+                return res.status(403).json({ message: 'Token is invalid or expired' });
+            }
+            req.user = decoded;
+            next();
+        });
+    } else {
+        next();
+    }
+});
+
+app.get('/', (req, res) => {
+    if (req.user) {
+        return res.json({
+            username: req.user.username,
+            logout: 'http://localhost:3000/logout'
+        });
+    }
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/logout', (req, res) => {
+    res.json({ message: 'Logged out' });
+});
+
 app.post('/api/login', (req, res) => {
     const { login, password } = req.body;
 
     const user = users.find((user) => {
-        if (user.login == login && user.password == password) {
-            return true;
-        }
-        return false
+        return user.login === login && user.password === password;
     });
 
     if (user) {
-        req.session.username = user.username;
-        req.session.login = user.login;
-
-        res.json({ token: req.sessionId });
+        const token = jwt.sign({ username: user.username }, JWT_SECRET_KEY, { expiresIn: JWT_EXPIRATION });
+        res.json({ token });
+    } else {
+        res.status(401).send();
     }
-
-    res.status(401).send();
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
